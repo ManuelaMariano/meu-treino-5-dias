3d804f9a0aee8eab1efd4c6b9dda64a9d1f76d07
Manuela Mariano <manu10mariano@gmail.com>
2026-09-15 07:38:07 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,18 @@
-const CACHE='meu-treino-v1';
+const CACHE='meu-treino-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request,copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
